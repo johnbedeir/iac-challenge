fe5f7f935751223e568cfc87aded6f00ff7f84d4
JohnyDev <john.bedeir@gmail.com>
2022-02-20 01:21:50 +0100

    comment the required version

--- terraform/us-east-2/versions.tf
+++ terraform/us-east-2/versions.tf
@@ -1,10 +1,10 @@
-terraform {
-  required_version = ">= 0.13.1"
+# terraform {
+#   required_version = ">= 0.13.1"
 
-  required_providers {
-    aws        = "~> 3.38"
-    kubernetes = "~> 2.1"
-    helm = "~> 2.1"
-    google = "~> 3.39.0"
-  }
-}
+#   required_providers {
+#     aws        = "~> 3.38"
+#     kubernetes = "~> 2.1"
+#     helm = "~> 2.1"
+#     google = "~> 3.39.0"
+#   }
+# }
